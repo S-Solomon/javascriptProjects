cba9ab506794d1f219e1a9bbdfefc0d36227cacd
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-02-11 15:19:01 +0000

    first commit

---
+++ hexCodeGenerator/script.js
@@ -0,0 +1,22 @@
+const btn = document.getElementById('change');
+const text = document.getElementById('color');
+
+const generator = function() {
+    newColor = '#' + (Math.random() * 0xFFFFFF<<0).toString(16);
+    
+    console.log(newColor.length);
+    if(newColor.length < 7) {
+        generator();
+    }
+}
+
+btn.addEventListener('click', function() {
+    generator();
+
+    document.body.style.background = newColor;
+    btn.style.color = newColor;
+    text.innerText = newColor;
+})
+
+
+
