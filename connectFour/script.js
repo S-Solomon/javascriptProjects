a8547da422bcb513722c01352c0ca0d23bf787f3
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-02-14 17:47:34 +0000

    log cell coordinates when clicked

--- connectFour/script.js
+++ connectFour/script.js
@@ -1,16 +1,26 @@
 
 const tableRow = document.getElementsByTagName('tr');
-const tableData = Array.from(document.getElementsByTagName('td'));
+const tableData = document.getElementsByTagName('td');
 const tableSlot = document.querySelector('.slot');
 const playerTurn = document.querySelector('.player-turn');
 const resetBtn = document.querySelector('.reset');
 
 // log cell coordinates when clicked
-// for (i = 0; i < tableData.length; i++) {
-//     tableData[i].addEventListener('click', (e) => {
+for (i = 0; i < tableData.length; i++) {
+    tableData[i].addEventListener('click', (e) => {
+        console.log(`${e.target.parentElement.rowIndex}, ${e.target.cellIndex}`)
+    });
+};
+
+
+// tableData.forEach(function(data) {
+//     data.addEventListener('click', (e) => {
 //         console.log(`${e.target.parentElement.rowIndex}, ${e.target.cellIndex}`)
 //     });
-// };
 
+// })
 
-console.log(tableData);
+// while(!player1){
+//     let player1 = prompt('Player One: Enter your name. You will be red');
+// }
+// player1Color = 'red'
