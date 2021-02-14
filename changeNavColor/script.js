7fe948ad137e2b9ebb633cfe63a0d4844cbcc2ed
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-02-14 08:30:10 +0000

    descriptive variable name

--- changeNavColor/script.js
+++ changeNavColor/script.js
@@ -1,8 +1,8 @@
 const header = document.querySelector('.header');
 
 window.onscroll = function() {
-    let top = window.scrollY;
-    if (top >=20 ? header.classList.add('active') : header.classList.remove('active'));
+    let scrollTop = window.scrollY;
+    if (scrollTop >=20 ? header.classList.add('active') : header.classList.remove('active'));
 }
 
 
