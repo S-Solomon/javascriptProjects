674ef0419f7a43abe445ebf380a1f8720e9f1dc8
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-02-14 19:13:37 +0000

    alternate players logic

--- connectFour/script.js
+++ connectFour/script.js
@@ -13,13 +13,6 @@ for (i = 0; i < tableData.length; i++) {
 };
 
 
-// tableData.forEach(function(data) {
-//     data.addEventListener('click', (e) => {
-//         console.log(`${e.target.parentElement.rowIndex}, ${e.target.cellIndex}`)
-//     });
-
-// })
-
 while(!player1){
     var player1 = prompt('Player One: Enter your name. You will be red');
 }
@@ -32,3 +25,29 @@ var player2Color = 'yellow'
 
 let currentPlayer = 1;
 playerTurn.textContent = `${player1}'s turn!`
+
+Array.prototype.forEach.call(tableData, (cell) => {
+    cell.addEventListener('click', changeColor);
+    // set all slots to white for new game
+    cell.style.backgroundColor = 'white';
+});
+
+function changeColor(e) {
+    let column = e.target.cellIndex;
+    let row = [];
+
+    for (let i = 5; i > -1; i--) {
+        if(tableRow[i].children[column].style.backgroundColor == 'white'){
+            row.push(tableRow[i].children[column]);
+            if(currentPlayer === 1) {
+                row[0].style.backgroundColor = player1Color;
+                playerTurn.textContent = `${player2}'s turn` ;
+                return currentPlayer = 2;
+            }else {
+                row[0].style.backgroundColor = player2Color;
+                playerTurn.textContent = `${player1}'s turn` ;
+                return currentPlayer = 1;
+            }
+        }
+    }
+}
