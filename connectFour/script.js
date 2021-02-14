c4e3d472e77478dd0cc85b8934e22d79455db55f
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-02-14 09:45:51 +0000

    visual

---
+++ connectFour/script.js
@@ -0,0 +1,14 @@
+
+const tableRow = document.getElementsByTagName('tr');
+const tableData = document.getElementsByTagName('td');
+const tableSlot = document.querySelector('.slot');
+const playerTurn = document.querySelector('.player-turn');
+const resetBtn = document.querySelector('.reset');
+
+// log cell coordinates when clicked
+for (i = 0; i < tableData.length; i++) {
+    tableData[i].addEventListener('click', (e) => {
+        console.log(`${e.target.parentElement.rowIndex}, ${e.target.cellIndex}`)
+    });
+};
+
