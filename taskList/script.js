d0079d6f72062ba101e25e40b5781020f495a457
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-02-16 10:23:55 +0000

    delete task from localstorage

--- taskList/script.js
+++ taskList/script.js
@@ -23,6 +23,7 @@ function addTask(e){
 function deleteTask(e) {
     if (confirm('Are you sure you wish to delete this item ?')) {
         e.target.remove()
+        deleteTaskfromLocalStorage(e.target);
     }
 }
 
@@ -62,5 +63,30 @@ function getTasksFromLocalStorage(){
         list.appendChild(li);
     })
 }
+
+function deleteTaskfromLocalStorage(taskItem){
+
+    let tasks;
+
+    if(localStorage.getItem('tasks') === null) {
+        tasks = [];
+    }else {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    } 
+
+    tasks.forEach(function(task, index) {
+        if(taskItem.textContent === task) {
+            tasks.splice(index, 1);
+        }
+    })
+
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+
+
+
+
+
 
 getTasksFromLocalStorage();
