0f16523134defeab979193045847d9423d9a1f5d
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-03-12 07:02:30 +0000

    change

--- Animated Hamburger Menu/script.js
+++ Animated Hamburger Menu/script.js
@@ -14,10 +14,30 @@ function openMenu() {
     navMenu.classList.toggle('active');
     navLeft.classList.toggle('active');
     navRight.classList.toggle('active');
-    contact.classList.toggle('active');
     logo.classList.toggle('active');
 
-    navLinks.forEach((link) => {
-        link.classList.toggle('active');
-    });
+    // Remove active class to restart delayed effect
+    contact.classList.remove('active');
+    navRight.classList.remove('active'); 
+
+    if(menuToggle.classList.contains('active')) {
+        setTimeout(() => {
+            navRight.classList.add('active')
+        }, 100)
+
+        for(let i = 0; i < navLinks.length; i ++) {
+            navLinks[i].classList.remove('active');
+            setTimeout(() => {
+                navLinks[i].classList.add('active');
+            }, i * 100)
+        }
+
+        setTimeout(() => {
+            contact.classList.add('active');
+        }, 700);
+    }
+
+    // navLinks.forEach((link) => {
+    //     link.classList.toggle('active');
+    // });
 };
