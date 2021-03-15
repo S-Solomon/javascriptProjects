2638a57d9c0b9ab2441554bd4834191f2bf50ee9
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-03-15 19:36:45 +0000

    minor

--- Animated Dark Mode Toggle/script.js
+++ Animated Dark Mode Toggle/script.js
@@ -1,9 +1,7 @@
 let modeToggle = document.querySelector('.mode-tog');
 let darkMode = document.querySelector('.dark-mode');
 
-console.log(modeToggle);
-
 modeToggle.addEventListener('click', () => {
     modeToggle.classList.toggle('active');
     darkMode.classList.toggle('active');
-})
+});
