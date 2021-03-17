6f7f7f65527bed17c23c8772ab166682109e93fd
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-03-17 16:27:29 +0000

    changes

--- Nav Image Hover Effect/script.js
+++ Nav Image Hover Effect/script.js
@@ -5,13 +5,15 @@ links.forEach((link, idx) => {
     link.addEventListener('mousemove', (e) => {
         link.children[1].style.opacity = 1;
         link.children[0].style.zIndex = 3;
-        link.children[1].style.transform = `translate(${e.clientX - 300}px, -${e.clientY / 3}px) rotate(${e.clientX / 20}deg)`;
+        link.children[1].style.transform = `translate(${e.clientX - 300}px, -${e.clientY / 3}px) rotate(${e.clientX / 50}deg)`;
         linkImages[idx].style.transform = 'scale(1, 1)';
+        link.style.zIndex = 2;
     })
 
     link.addEventListener('mouseleave', (e) => {
         link.children[1].style.opacity = 0;
         link.children[1].style.transform = `translate(${-e.clientX}px, -300px)`;
         linkImages[idx].style.transform = 'scale(0.8, 0.8)';
+        link.style.zIndex = 0;
     })
 })
