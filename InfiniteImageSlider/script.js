c19d534219cb04850e762600da2d3f6e1de6e37f
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-03-25 15:34:34 +0000

    script

---
+++ InfiniteImageSlider/script.js
@@ -0,0 +1,60 @@
+let sliderWrap = document.querySelector('.slider-wrap');
+let slider = document.querySelector('.slider');
+let clonesWidth;
+let sliderWidth;
+let clones = [];
+let disabledScroll = false;
+let scrollPosition;
+
+let items = [...document.querySelectorAll('.slider-item')];
+let images = [...document.querySelectorAll('.img-div')];
+
+images.forEach((image, idx) => {
+    image.style.backgroundImage = `url(./image/${idx + 1}.jpg)`
+})
+
+items.forEach(item => {
+    let clone = item.cloneNode(true);
+    clone.classList.add('clone');
+    slider.appendChild(clone);
+    clones.push(clone);
+})
+
+function getClonesWidth() {
+    let width = 0;
+    clones.forEach(clone => {
+        width += clone.offsetWidth;
+    })
+    return width;
+}
+
+function getScrollPos() {
+    return window.scrollY;
+}
+
+function scrollUpdate() {
+    scrollPosition = getScrollPos();
+    if(clonesWidth + scrollPosition >= sliderWidth){
+        window.scrollTo({top: 1});
+    }else if (scrollPosition <= 0) {
+        window.scrollTo({top: sliderWidth - clonesWidth - 1});
+    }
+
+    slider.style.transform = `translateX(${-window.scrollY}px)`
+
+    requestAnimationFrame(scrollUpdate);
+}
+
+function onLoad() {
+    calculateDimensions()
+    document.body.style.height = `${sliderWidth}px`
+    window.scrollTo({top: 1});
+    scrollUpdate();
+}
+
+function calculateDimensions() {
+    sliderWidth = slider.getBoundingClientRect().width;
+    clonesWidth = getClonesWidth();
+}
+
+onLoad()
