659342aa400e2c3ee32f627167bc0b1ac066c6f2
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-03-31 14:20:00 +0100

    parallax effect

--- horizontalParallaxSlider/script.js
+++ horizontalParallaxSlider/script.js
@@ -6,6 +6,9 @@ let current = 0;
 let target = 0;
 let ease = 0.05;
 
+window.addEventListener('resize', init); //make sure the slider always fit in every window size
+
+
 images.forEach((img, idx) => {
     img.style.backgroundImage = `url(./images/${idx+1}.jpg)`;
 })
@@ -28,10 +31,21 @@ function animate() {
     current = parseFloat(lerp(current, target, ease)).toFixed(2);
     target = window.scrollY;
     setTransform(slider, `translateX(-${current}px)`)
+    animateImages()
     requestAnimationFrame(animate);
 }
 
 init()
 animate()
 
+// Parallax Effect
 
+function animateImages() {
+    let ratio = current / imageWidth;
+    let intersectionRatioValue;
+
+    images.forEach((image, idx) => {
+        intersectionRatioValue = ratio - (idx * 0.7); // experiment with these numbers
+        setTransform(image, `translateX(${intersectionRatioValue * 70}px)`)
+    })
+}
