d76a86feb27c6e4512162a73d1e7ed9fa712f569
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-04-22 08:40:08 +0100

    setupcamera

--- THREEJS-imageRGB-Split/js/app.js
+++ THREEJS-imageRGB-Split/js/app.js
@@ -27,5 +27,45 @@ function smoothScroll() {
 }
 
 
+class EffectCanvas {
+    constructor() {
+        this.container = document.querySelector('main');
+        this.images = [...document.querySelectorAll('img')];
+        this.meshItems = []; // used to store all meshes we will be creating
+        this.setupCamera();
+        this.createMeshItems();
+        this.render();
+    }
+
+    get viewport() {
+        let width = window.innerWidth;
+        let height = window.innerHeight;
+        let aspectRatio = width / height;
+        return {
+            width,
+            height,
+            aspectRatio
+        }
+    }
+
+    setupCamera() {
+
+        window.addEventListener('resize', this.onWindowResize.bind(this));
+
+        this.scene = new THREE.Scene();
+
+        // perspective camera
+        let perspective = 1000;
+        const fov = (180 * (2 * Math.atan(window.innerHeight / 2 / perspective))) / Math.PI;
+        this.camera = new THREE.PerspectiveCamera(fov, this.viewport.aspectRatio, 1, 1000);
+        this.camera.position.set(0, 0, perspective)
+
+        this.renderer = new THREE.WebGL1Renderer({antialias: true, alpha: true});
+        this.renderer.setSize(this.viewport.width, this.viewport.height);
+        this.renderer.setPixelRatio(window.devicePixelRatio);
+        this.container.appendChild(this.renderer.domElement);
+    }
+}
+
 init()
 smoothScroll()
